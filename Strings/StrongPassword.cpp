3f32576111aa25ecef73ad4b64ae44d9eae69c5b
Vidit Shah <vidit0210@outlook.com>
2020-06-29 00:27:11 +0530

    TwoCharacters

--- Strings/StrongPassword.cpp
+++ Strings/StrongPassword.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <algorithm>
 using namespace std;
-
 int check(string s, string w)
 {
   for (char c : s)
@@ -17,20 +16,19 @@ int check(string s, string w)
 
 int main()
 {
-  int n;
-  cin >> n;
   string s;
-  cin >> s;
+  int n;
+  cin >> n >> s;
+
   string numbers = "0123456789";
   string lower_case = "abcdefghijklmnopqrstuvwxyz";
   string upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   string special_characters = "!@#$%^&*()-+";
-  int ans = 0;
-  ans += check(s, numbers);
-  ans += check(s, lower_case);
-  ans += check(s, upper_case);
-  ans += check(s, special_characters);
-  ans = max(ans, 6 - n);
-  cout << ans << endl;
-  return 0;
+  int missing = 0;
+  missing += check(s, numbers);
+  missing += check(s, lower_case);
+  missing += check(s, upper_case);
+  missing += check(s, special_characters);
+  int res = max(missing, 6 - n);
+  cout << res << endl;
 }
