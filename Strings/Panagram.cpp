5952bbc9ea01d1323bf97f08f2f9fa4d465e70d2
Vidit Shah <vidit0210@outlook.com>
2020-06-29 02:21:11 +0530

    weighted Uniform

--- Strings/Panagram.cpp
+++ Strings/Panagram.cpp
@@ -1,35 +1,25 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 #include <unordered_set>
 using namespace std;
 
 int main()
 {
 
-  unordered_set<char> letters;
-
+  unordered_set<char> s;
   for (char c = 'a'; c <= 'z'; c++)
   {
-    letters.insert(c);
+    s.insert(c);
   }
-
   char c;
   while (cin >> c)
   {
     c = tolower(c);
-    letters.erase(c);
-    if (letters.size() == 0)
+    s.erase(c);
+    if (s.size() == 0)
     {
       cout << "pangram";
       return 0;
     }
   }
-
   cout << "not pangram";
-
-  /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-  return 0;
 }
