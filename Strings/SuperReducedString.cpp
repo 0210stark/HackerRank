05c4a7b681b6abe8ad831b7ff7b95250556fb956
Vidit Shah <0210vidit@gmail.com>
2020-06-28 20:43:10 +0530

    SuperReducedString

--- Strings/SuperReducedString.cpp
+++ Strings/SuperReducedString.cpp
@@ -1,4 +1,23 @@
-//
-// Created by vidit shah on 28/06/20.
-//
+#include <string>
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 1; i < s.length(); i++)
+    {
+        if (s[i] == s[i - 1])
+        {
+            s.erase(i - 1, 2);
+            i = 0;
+        }
+    }
+    if (s.length() == 0)
+        cout << "Empty String";
+    else
+        cout << s;
+
+    return 0;
+}
